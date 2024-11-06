9db52e15199f01c2c7e167c723375fc2af553989
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-11-06 12:58:14 +0100

    add theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,49 @@
+import { useEffect, useState } from "react";
 import logo from "../assets/logo.svg";
 import "./Header.css";
 
-export const Header = () =>{
-    return(
-        <header>
-            <div className="logo">
-                <img src={logo} alt="Taskmate logo"></img>
-                <span>Taskmate</span>
-            </div>
-            <div className="themeSelector">
-                <span className="light"></span>
-                <span className="medium activeTheme"></span>
-                <span className="dark"></span>
-                <span className="gOne"></span>
-                <span className="gTwo"></span>
-            </div>
-        </header>
-    )
-}
+export const Header = () => {
+  const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme')) || "medium");
+
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+    document.documentElement.removeAttribute("class");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
+
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt="Taskmate logo"></img>
+        <span>Taskmate</span>
+      </div>
+      <div className="themeSelector">
+        <span
+          className={theme === "light" ? "light activeTheme" : "light"}
+          onClick={() => setTheme("light")}
+        ></span>
+        <span
+          className={theme === "medium" ? "medium activeTheme" : "medium"}
+          onClick={() => setTheme("medium")}
+        ></span>
+        <span
+          className={theme === "dark" ? "dark activeTheme" : "dark"}
+          onClick={() => setTheme("dark")}
+        ></span>
+        <span
+          className={theme === "gOne" ? "gOne activeTheme" : "gOne"}
+          onClick={() => setTheme("gOne")}
+        ></span>
+        <span
+          className={theme === "gTwo" ? "gTwo activeTheme" : "gTwo"}
+          onClick={() => setTheme("gTwo")}
+        ></span>
+        <span
+          className={theme === "gThree" ? "gThree activeTheme" : "gThree"}
+          onClick={() => setTheme("gThree")}
+        ></span>
+      </div>
+    </header>
+  );
+};
